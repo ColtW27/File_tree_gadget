fae3e87c9181654546418a40bbe36002a59eaa5a
Colton Wilmouth <cwilmouth27@yahoo.com>
2021-07-21 13:48:16 -0400

    Create folder and file components

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import React from 'react';
 function App() {
   return <div>
       
-      <Folder />
-      <Folder />
-      <Folder />
+    <Folder name = "Desktop" />
+    <Folder name = "Applications" />
+    <File name = "dogs.jpeg" />
+    <File name = "cats.png" />
 
     </div>
 }
 
 
-const Folder = () => {
-  return <h4>Folder</h4>
+const Folder = (props) => {
+  return <h4>{props.name}</h4>
 }
+
+const File = (props) => {
+    return <div>{props.name}</div>
+}
+
+
 export default App;
